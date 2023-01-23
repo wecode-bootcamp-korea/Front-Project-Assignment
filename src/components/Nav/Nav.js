0ae310804d06f3e4265dd217a62a434a621238a5
김기윤 <ki@Kis-MacBook-Pro.local>
2023-01-24 01:41:40 +0900

    Add: 검색기능 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Nav.scss';
 
@@ -13,6 +13,16 @@ const Nav = () => {
 
   const goToDetail = id => {};
 
+  useEffect(() => {
+    fetch('https://dummyjson.com/products/search?q=')
+      .then(response => response.json())
+      .then(result => setSearchList(result.products));
+  }, []);
+
+  const searchItem = searchList.filter(keyword =>
+    keyword.title.toLowerCase().includes(searchText)
+  );
+
   return (
     <nav className="nav">
       <div className="contentContainer">
@@ -26,19 +36,22 @@ const Nav = () => {
             value={searchText}
             onChange={searchProdut}
           />
-          <div className="searchBox">
-            {searchList.map(list => {
-              return (
-                <span
-                  key={list.id}
-                  className="result"
-                  onClick={() => goToDetail(list.id)}
-                >
-                  {list.title}
-                </span>
-              );
-            })}
-          </div>
+          {searchText.length > 0 && (
+            <div className="searchBox">
+              {searchItem.length > 0 &&
+                searchItem.map(list => {
+                  return (
+                    <span
+                      key={list.id}
+                      className="result"
+                      onClick={() => goToDetail(list.id)}
+                    >
+                      {list.title}
+                    </span>
+                  );
+                })}
+            </div>
+          )}
         </div>
         <Link className="loginBtn" to="/login">
           로그인
