64035c33d00d52974316de8c17dc10c6fd70158e
laralina <laralina@laralinaui-MacBook-Pro.local>
2023-02-22 22:25:57 +0900

    ADD: 로그인회원가입/캐러샐 외 완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Nav.scss';
 
 const Nav = () => {
   const [searchText, setSearchText] = useState('');
   const [searchList, setSearchList] = useState([]);
+  const [productData, setProductData] = useState([]);
+
   const navigate = useNavigate();
 
-  const searchProdut = e => {
+  useEffect(() => {
+    fetch('https://dummyjson.com/products/search?q= ')
+      .then(res => res.json())
+      .then(data => {
+        setProductData(data.products);
+      });
+  }, []);
+
+  const searchProduct = e => {
     setSearchText(e.target.value);
+    setSearchList(
+      productData &&
+        productData.filter(
+          el => el && el.title.toLowerCase().includes(searchText.toLowerCase())
+        )
+    );
   };
 
-  const goToDetail = id => {};
+  const goToDetail = id => {
+    navigate(`/detail/${id}`);
+    setSearchList([]);
+  };
 
   return (
     <nav className="nav">
@@ -24,20 +43,21 @@ const Nav = () => {
             className="searchInput"
             placeholder="검색"
             value={searchText}
-            onChange={searchProdut}
+            onChange={searchProduct}
           />
           <div className="searchBox">
-            {searchList.map(list => {
-              return (
-                <span
-                  key={list.id}
-                  className="result"
-                  onClick={() => goToDetail(list.id)}
-                >
-                  {list.title}
-                </span>
-              );
-            })}
+            {searchList &&
+              searchList.map(list => {
+                return (
+                  <span
+                    key={list.id}
+                    className="result"
+                    onClick={() => goToDetail(list.id)}
+                  >
+                    {list.title}
+                  </span>
+                );
+              })}
           </div>
         </div>
         <Link className="loginBtn" to="/login">
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,47 +1,80 @@
 import React, { useEffect, useState } from 'react';
+import {
+  useNavigate,
+  useLocation,
+  useSearchParams,
+  Link,
+} from 'react-router-dom';
 import './Products.scss';
 
 const Products = () => {
   const [productsList, setProductsList] = useState([]);
   const [totalProduct, setTotalProduct] = useState(0);
+  const [choiceParams, setChoiceParams] = useState('all');
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const skip = searchParams.get('skip');
+  const limit = 10;
+
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://dummyjson.com/products')
+    fetch(`https://dummyjson.com/products?skip=${skip}&limit=${limit}`)
       .then(res => res.json())
       .then(data => {
         setProductsList(data.products);
         setTotalProduct(Math.floor(data.total / 10));
       });
-  }, []);
+  }, [skip]);
 
-  const filterProducts = e => {};
+  const movePage = pageNumber => {
+    searchParams.set('skip', (pageNumber - 1) * limit);
+    setSearchParams(searchParams);
+  };
+
+  const filterProducts = e => {
+    setChoiceParams(choiceParams => e.target.value);
+    searchParams.set('sort', `${choiceParams}`);
+    setSearchParams(searchParams => searchParams);
+  };
 
   return (
     <div className="products">
       <div className="filterContainer">
         <select onChange={filterProducts}>
-          <option value="">옵션을 선택해주세요</option>
+          <option value="all">옵션을 선택해주세요</option>
           <option value="price">가격 낮은순</option>
           <option value="rating">별점 높은순</option>
           <option value="discountPercentage">할인율 높은순</option>
         </select>
       </div>
       <div className="listWrap">
-        {productsList.map(list => {
-          return (
-            <div key={list.id} className="cardContainer">
-              <img alt={list.title} className="cardImage" />
-              <div className="contentBox">
-                <span>상품명 : </span>
-                <span>가격 : $</span>
-                <span>별점 : </span>
-                <span>할인율 : %</span>
-              </div>
-            </div>
-          );
-        })}
+        {productsList &&
+          productsList.map(list => {
+            return (
+              <Link key={list.id} to={`/detail/${list.id}`}>
+                <div key={list.id} className="cardContainer">
+                  <img
+                    alt={list.title}
+                    className="cardImage"
+                    src={list.thumbnail}
+                  />
+                  <div className="contentBox">
+                    <span>상품명 : {list.title}</span>
+                    <span>가격 : {list.price}$</span>
+                    <span>별점 : {list.rating}</span>
+                    <span>할인율 : {list.discountPercentage}%</span>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
       </div>
-      <div className="buttonWrap" />
+      <div className="buttonWrap">
+        <button onClick={() => movePage(1)}>1</button>
+        <button onClick={() => movePage(2)}>2</button>
+        <button onClick={() => movePage(3)}>3</button>
+      </div>
     </div>
   );
 };
