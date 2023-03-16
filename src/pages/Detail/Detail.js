10b36529e269b0227030e8463b9752729cd74db0
sstaar91 <sstaar91@gmail.com>
2023-03-17 01:05:42 +0900

    상세페이지 세팅

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,42 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
 import './Detail.scss';
 
 const Detail = () => {
   const [product, setProduct] = useState({});
   const [quantity, setQuantity] = useState(1);
-  const params = useParams();
-
-  useEffect(() => {
-    fetch(`https://dummyjson.com/products`)
-      .then(res => res.json())
-      .then(data => setProduct(data));
-  }, []);
 
   const calculateQuantity = e => {};
 
-  const addCart = () => {
-    fetch('https://dummyjson.com/carts/add', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        userId: 1,
-        products: [
-          {
-            id: 1,
-            quantity: 1,
-          },
-        ],
-      }),
-    })
-      .then(res => res.json())
-      .then(data => console.log(data));
-  };
+  const addCart = () => {};
 
   return (
     <div className="detail">
       <div className="imageContainer">
-        <img src="" alt={product.title} />
+        <img src="" alt="" />
       </div>
       <div className="productContent">
         <span className="title">타이틀</span>
@@ -45,7 +21,7 @@ const Detail = () => {
         <span className="price">가격 : $</span>
         <span className="subInfo">평점 : </span>
         <div className="handleBox">
-          <span className="quantity"> 수량 : 개</span>
+          <span className="quantity"> 수량 : {quantity}개</span>
           <button
             className="quantityBtn"
             name="plus"
