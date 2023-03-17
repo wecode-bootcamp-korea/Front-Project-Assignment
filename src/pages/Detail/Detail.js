f4dbef8db775bd96582f45439aa20b34d75261be
sstaar91 <sstaar91@gmail.com>
2023-03-17 19:20:48 +0900

    ADD : 디테일 페이지 구현

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,13 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import './Detail.scss';
 
 const Detail = () => {
   const [product, setProduct] = useState({});
   const [quantity, setQuantity] = useState(1);
 
-  const calculateQuantity = e => {};
+  const params = useParams();
+  const dialogRef = useRef();
+  const navigate = useNavigate();
 
-  const addCart = () => {};
+  useEffect(() => {
+    fetch('')
+      .then(res => res.json())
+      .then(data => setProduct(data));
+  }, []);
+
+  const calculateQuantity = () => {};
+
+  const handleModal = e => {
+    if (e.target.value === 'close') return;
+    navigate('/cart');
+  };
+
+  const addCart = () => {
+    fetch('', {
+      method: '',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userId: 1,
+        products: [
+          {
+            id: '',
+            quantity: '',
+          },
+        ],
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.id) {
+          dialogRef.current.showModal();
+        }
+      });
+  };
 
   return (
     <div className="detail">
@@ -21,7 +57,7 @@ const Detail = () => {
         <span className="price">가격 : $</span>
         <span className="subInfo">평점 : </span>
         <div className="handleBox">
-          <span className="quantity"> 수량 : {quantity}개</span>
+          <span className="quantity"> 수량 : 개</span>
           <button
             className="quantityBtn"
             name="plus"
@@ -41,6 +77,17 @@ const Detail = () => {
           장바구니 담기
         </button>
       </div>
+      <dialog ref={dialogRef} className="alertModal">
+        <span>장바구니 페이지로 이동하겠습니까?</span>
+        <form method="dialog" className="buttonWrap">
+          <button value="confirm" onClick={handleModal} className="modalBtn">
+            네
+          </button>
+          <button value="close" onClick={handleModal} className="modalBtn">
+            아니오
+          </button>
+        </form>
+      </dialog>
     </div>
   );
 };
