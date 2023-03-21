1b18f10066bff2ec34f4faeaeb074b750233717d
jinp1993 <jinp1993@naver.com>
2023-03-21 10:06:58 +0900

    MODIFY : 캐러셀 도트 클릭시 이미지 이동 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Carousel from './Carousel/Carousel';
 import './Main.scss';
 
 const Main = () => {
-  return <section className="main" />;
+  return (
+    <section className="main">
+      <div className="carousel">
+        <Carousel />
+      </div>
+    </section>
+  );
 };
 
 export default Main;
