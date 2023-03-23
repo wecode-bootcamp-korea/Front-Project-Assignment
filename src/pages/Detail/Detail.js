11e8725bcc9eb997e56bde39bb5100656aeeff57
summer-o3o <ho_ho95@naver.com>
2023-03-23 10:56:50 +0900

    update : 장바구니 담기

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -35,15 +35,15 @@ const Detail = () => {
   };
 
   const addCart = () => {
-    fetch('', {
-      method: '',
+    fetch('https://dummyjson.com/carts/add', {
+      method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         userId: 1,
         products: [
           {
-            id: '',
-            quantity: '',
+            id: `${productId}`,
+            quantity: `${quantity}`,
           },
         ],
       }),
