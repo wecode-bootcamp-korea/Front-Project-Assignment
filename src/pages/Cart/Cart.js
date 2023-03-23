a242c1446cab419fcf3b3f0348a6fc4051dff541
summer-o3o <ho_ho95@naver.com>
2023-03-23 19:06:52 +0900

    update : 체크박스 구현

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,8 +1,10 @@
+import { check } from 'prettier';
 import React, { useEffect, useState } from 'react';
 import './Cart.scss';
 
 const Cart = () => {
   const [cartList, setCartList] = useState([]);
+  const [checkList, setCheckList] = useState([]);
 
   useEffect(() => {
     fetch('https://dummyjson.com/carts/1')
@@ -12,17 +14,43 @@ const Cart = () => {
       });
   }, []);
 
-  const isAllChecked = '';
+  const handleAllCheck = checked => {
+    if (checked) {
+      let arr = [];
+      cartList.map(item => arr.push(item.id));
+      setCheckList(arr);
+    } else {
+      setCheckList([]);
+    }
+  };
+
+  const handleChecked = e => {
+    const id = Number(e.target.name);
+    if (checkList.includes(id)) {
+      setCheckList([...checkList.filter(arrId => arrId !== id)]);
+    } else {
+      setCheckList([...checkList, Number(id)]);
+    }
+  };
 
   return (
     <div className="cart">
-      <input type="checkbox" checked={isAllChecked} />
+      <input
+        type="checkbox"
+        checked={cartList.length === checkList.length ? 'checked' : ''}
+        onClick={e => handleAllCheck(e.target.checked)}
+      />
       전체선택
       <div className="line" />
       {cartList.map(product => {
         return (
-          <div className="productBox" key="">
-            <input type="checkbox" checked="" />
+          <div className="productBox" key={`${product.id}`}>
+            <input
+              type="checkbox"
+              checked={checkList.includes(product.id) ? 'checked' : ''}
+              name={`${product.id}`}
+              onClick={handleChecked}
+            />
             <span className="title">{product.title}</span>
             <span>{product.price}$</span>
             <span>{product.quantity}개</span>
