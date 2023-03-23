951691c9e47658a493b4c5758494f71b1fbb9d83
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-03-23 14:26:39 +0900

    cart 진행중

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -3,29 +3,35 @@ import './Cart.scss';
 
 const Cart = () => {
   const [cartList, setCartList] = useState([]);
+  const [checkList, setCheckList] = useState(false);
 
   useEffect(() => {
-    fetch('')
+    fetch('https://dummyjson.com/carts/1')
       .then(res => res.json())
       .then(data => {
         setCartList(data.products);
       });
   }, []);
 
-  const isAllChecked = '';
+  if (!cartList) return null;
 
   return (
     <div className="cart">
-      <input type="checkbox" checked={isAllChecked} />
+      <input type="checkbox" checked={false} onClick="checked" />
       전체선택
       <div className="line" />
-      {cartList.map(product => {
+      {cartList.map(cartList => {
         return (
           <div className="productBox" key="">
-            <input type="checkbox" checked="" />
-            <span className="title">타이틀</span>
-            <span>$</span>
-            <span>개</span>
+            <input
+              type="checkbox"
+              onChange={setCheckList}
+              onClick="checked"
+              checked={false}
+            />
+            <span className="title">{cartList.title}</span>
+            <span>{cartList.price}</span>
+            <span>{cartList.quantity}</span>
           </div>
         );
       })}
